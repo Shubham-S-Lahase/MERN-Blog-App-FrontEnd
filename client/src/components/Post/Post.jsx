2439667f5e1045609f2e49a17ac66aadd2242cc5
Shubham <shubhamlahase@hmail.com>
2023-02-08 19:43:11 +0530

    added navigation using Link to the image and postheader

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import './Post.css'
-import { format } from 'date-fns';
-const Post = ({title, summary, cover, content, createdAt, author}) => {
-    return(
-        <div className="post">
-          <div className="image">
-            <img
-              src={`http://localhost:4000/${cover}`}
-              alt=""
-            />
-          </div>
-          <div className="text">
-            <h2>{title}</h2>
-            <p className="info">
-              <a className="author">{author.username}</a>
-              <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
-            </p>
-            <p className="summary">{summary}</p>
-          </div>
-        </div>
-    );
+import "./Post.css";
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
+const Post = ({_id,title, summary, cover, content, createdAt, author }) => {
+  return (
+    <div className="post">
+      <div className="image">
+        <Link id="viewPost" to={`/post/${_id}`}>
+          <img src={`http://localhost:4000/${cover}`} alt="post-image" />
+        </Link>
+      </div>
+      <div className="text">
+        <Link id="viewPost" to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{format(new Date(createdAt), "MMM d, yyyy HH:mm")}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
